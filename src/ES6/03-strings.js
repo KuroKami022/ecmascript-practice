30a3b0ab815105c21a1e85da350ac132a9882847
Kami <luisru693@gmail.com>
2023-09-15 07:52:22 -0400

    let const var / arrow functions - strings

---
+++ src/ES6/03-strings.js
@@ -0,0 +1,19 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world + '!';
+
+// TEMPLATE LITERALS
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//MULTI LINE STRINGS
+
+let lorem = 'Esto es un string \n ' + 'Esto es otro string';
+
+let lorem2 = `Esta es una frase epica
+    y esta la continuacion de esa frase epica.
+`;
+
+console.log(lorem);
+console.log(lorem2);
